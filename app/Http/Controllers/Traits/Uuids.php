d0ae3983fef60e5ce704bea21c8f09b978782354
4kula <oce4n@protonmail.com>
2017-11-03 17:27:20 -0600

    implementing first trai

---
+++ app/Http/Controllers/Traits/Uuids.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+/**
+ *
+ */
+trait Uuids
+{
+    /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Uuid::generate()->string;
+        });
+    }
+}
