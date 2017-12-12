a017bf848cb152efa96c1fba45c40c82dbc5c1fb
4kula <oce4n@protonmail.com>
2017-12-12 10:17:56 -0700

    wip for abathur prompt

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -43,9 +43,10 @@ class DatabaseSeeder extends Seeder
     {
         $tasks = [
             '2017-10-28' => "Artanis needs to send important messages secretly to Zeratul, but the Zerg swarm is ever present and picking off their scouts ...\nExecutor, you have been tasked with creating a function/script in PHP to encrypt and decrypt any given sentence(string) for securing communication between them!\nYou have until 11/5/2017 to complete this!\nEn taro adun!",
-            '2017-12-04' => "Raynor is constantly bored on the Hyperion. He wants a guess the number game ... build it!",
+            // '0000-00-00' => "Raynor is constantly bored on the Hyperion. He wants a guess the number game ... build it!",
             // '0000-00-00' => "Zeratul wants to see the NutCracker performance, but he forgot his wallet at home, but home I mean his spceship the Voidseeker .. which is on permanent warp jump ... the formula for the next destination is x*(x^9)+x/x(x%2) where x is used for the next destination; find the 40th destination...",
             // '0000-00-00' => "Mojo is baking pies for a weekend party with the conclave. He wants to make sure he has enough doughnuts for eveyone. But he is not sure how many protoss bretheren will warp to his party .. assuming each protoss will eat (p*rand(1-4)); come up with the most cost efficient doughnut counts(least # doughnuts left over + least amount of $ spent)",
+            // abathur -> most efficient random proble, he x2 on production of rand(x-x) use abathur the most proficiently as possible
         ];
         foreach ($tasks as $date => $prompt) {
             DB::table('academy_tasks')->insert([
