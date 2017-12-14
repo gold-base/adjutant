80fa964ecb6dd21d472e6ba988272379796b3dfc
4kula <oce4n@protonmail.com>
2017-12-13 21:34:58 -0700

    abathru prompt complete

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -47,6 +47,7 @@ class DatabaseSeeder extends Seeder
             // '0000-00-00' => "Zeratul wants to see the NutCracker performance, but he forgot his wallet at home, but home I mean his spceship the Voidseeker .. which is on permanent warp jump ... the formula for the next destination is x*(x^9)+x/x(x%2) where x is used for the next destination; find the 40th destination...",
             // '0000-00-00' => "Mojo is baking pies for a weekend party with the conclave. He wants to make sure he has enough doughnuts for eveyone. But he is not sure how many protoss bretheren will warp to his party .. assuming each protoss will eat (p*rand(1-4)); come up with the most cost efficient doughnut counts(least # doughnuts left over + least amount of $ spent)",
             // abathur -> most efficient random proble, he x2 on production of rand(x-x) use abathur the most proficiently as possible
+            // '0000-00-00' => "Abathur is building a new zerg nest. A fully complete zerge next requires 30000 units of work. Assuming each drone can do rand(1-10) units of work, and if abathur pairs with a drone the production is x2, write a script/loop/function/whatever to come up with the most efficient drone usage to construct a new nest.",
         ];
         foreach ($tasks as $date => $prompt) {
             DB::table('academy_tasks')->insert([
